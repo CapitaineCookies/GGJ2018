e2c9c095d37e02f08d7db74173b8ff065f71b651
Maxence <maxou70@gmail.com>
2018-01-27 01:22:10 +0100

    first gamePlay
    Ajout du premier element de gameplay : Appuie dans un certain ordre

---
+++ Assets/Prefab/PushedButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushedButton : MonoBehaviour
+{
+
+	private bool lockButton = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+
+	void OnMouseUpAsButton ()
+	{
+		if (lockButton) {
+			return;
+		}
+		lockButton = true;
+		SquenceValidator squenceValidator = GetComponentInParent<SquenceValidator> ();
+		if (squenceValidator.IsComplete ()) {
+			return;
+		}
+		SpriteRenderer renderer = GetComponent<SpriteRenderer> ();
+		Color color = renderer.color;
+		if (squenceValidator.PushButton (this.gameObject)) {
+			renderer.color = Color.green;
+		} else {
+			renderer.color = Color.red;
+		}
+		StartCoroutine (resetColor(color));
+
+	}
+
+	IEnumerator resetColor (Color color)
+	{
+		yield return new WaitForSeconds (1.0f);
+		GetComponent<SpriteRenderer> ().color = color;
+		lockButton = false;
+	}
+}
---
+++ Assets/Script/SquenceValidator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquenceValidator : MonoBehaviour {
+
+	public List<GameObject> buttons;
+	// Nombre d'element dans la combinaison
+	public int combinaisonSize = 3;
+
+	public List<int> orders;
+	public int current = 0;
+
+	// Use this for initialization
+	void Start () {
+		int size = buttons.Count;
+		orders = new List<int>();
+		for (int i = 0; i < combinaisonSize; i++) {
+			orders.Add (Random.Range(0, buttons.Count));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/**
+	 * Valide le boutton. 
+	 * Si c'est le bon, on passe on passe au boutton suivant
+	 * Si c'etait le dernier, le groupe est valide
+	 * Si c'est le mauvais, reinicialise la secance
+	 **/
+	public bool PushButton(GameObject theObject) {
+		if(IsComplete()) {
+			return false;
+		}
+		if (theObject.Equals (GetNextButton ())) {
+			current++;
+			return true;
+		} else {
+			current = 0;
+			return false;
+		}
+	}
+
+	// Si true : le groupe est valide
+	public bool IsComplete() {
+		return current >= orders.Count;
+	}
+
+	// Retourne le prochain boutton a valider
+	GameObject GetNextButton() {
+		return buttons[orders[current]];
+	}
+
+}
