4ed2665ebfea6aaf2a17ea4f762e3a3ba28031f7
Maxence <maxou70@gmail.com>
2018-01-27 19:32:26 +0100

    Ajout boutton selecter

---
+++ Assets/Script/Selecter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecter : MonoBehaviour {
+
+	public int selectedPosition;
+
+	private SelecterRenderer selecterRenderer;
+
+	void Awake() {
+		selecterRenderer = GetComponent<SelecterRenderer> ();
+	}
+
+	void OnMouseUpAsButton ()
+	{
+		incSelectedPosition ();
+	}
+
+	private void incSelectedPosition() {
+		selectedPosition = (selectedPosition + 1) % selecterRenderer.Count();
+	}
+
+}
---
+++ Assets/Script/SelecterRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelecterRenderer : MonoBehaviour {
+
+	public List<Sprite> sprites;
+	private SpriteRenderer spritRenderer;
+	private Selecter selecter;
+
+	public void Awake() {
+		spritRenderer = GetComponent<SpriteRenderer>();
+		selecter = GetComponent<Selecter>();
+	}
+
+	public void Update() {
+		spritRenderer.sprite = sprites [selecter.selectedPosition];
+	}
+
+	public int Count () {
+		return sprites.Count;
+	}
+
+}
