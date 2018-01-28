74d41f916ab756cb2f89143a69fae26c789abeee
dunin <dunin@laposte.net>
2018-01-28 13:45:33 +0100

    add music intro

---
+++ Assets/Script/MusicStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MusicStart : MonoBehaviour {
+	private AudioSource audioSrc;
+
+
+	// Use this for initialization
+	void Start () {
+		audioSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
