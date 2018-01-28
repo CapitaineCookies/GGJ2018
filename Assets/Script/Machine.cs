76a602b8e2b5aed91a1dfdb29f9d9231befa812b
Maxence <maxou70@gmail.com>
2018-01-28 16:50:41 +0100

    Add condition fin

--- Assets/Script/Machine.cs
+++ Assets/Script/Machine.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Machine : MonoBehaviour {
 
-	
+	public AudioClip stressMusic;
+
+	private AudioSource audioSource;
+	private float duration;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+		duration = audioSource.clip.length;
+
+	}
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.timeSinceLevelLoad >= duration) {
+			if (checkVictory ()) {
+				SceneManager.LoadScene ("victory");
+			} else {
+				SceneManager.LoadScene ("carnetDessin");
+			}
+		}
 	}
 
-	void Save() {
-		SaveLoadManager.Save (this);
+	bool checkVictory() {
+		Validable[] validables = GetComponentsInChildren<Validable> ();
+		foreach(Validable validable in validables) {
+			if (!validable.IsValid()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
