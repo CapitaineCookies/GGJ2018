b9c7c3038db1c8d9ff3832b6b194426f0b95ffa5
Maxence <maxou70@gmail.com>
2018-01-28 13:14:33 +0100

    Add Img

---
+++ Assets/Script/unipolarSwitch/UnipolarSwitchRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnipolarSwitchRenderer : MonoBehaviour {
+
+	public Sprite switchRelease;
+	public Sprite switchPushed;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeSwitch(UnipolarSwitchState state) {
+		switch (state) {
+		case UnipolarSwitchState.RELEASE: 
+			spriteRenderer.sprite = switchRelease;
+			break;
+		case UnipolarSwitchState.PUSHED: 
+			spriteRenderer.sprite = switchPushed;
+			break;
+		}
+	}
+}
+
+public enum UnipolarSwitchState { RELEASE, PUSHED }
