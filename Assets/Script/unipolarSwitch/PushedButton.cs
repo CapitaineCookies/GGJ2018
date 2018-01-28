01863c3de69b7c763ad1ac37ce34fadc826f0a57
Maxence <maxou70@gmail.com>
2018-01-28 14:00:53 +0100

    Changement sequence group

---
+++ Assets/Script/unipolarSwitch/PushedButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushedButton : MonoBehaviour
+{
+	private UnipolarSwitchRenderer switchRenderer;
+	private SquenceValidator squenceValidator;
+
+	public Color feedBackColor = Color.grey;
+	public float feedbackDuration = 0.4f;
+
+	void Awake() {
+		squenceValidator = GetComponentInParent<SquenceValidator> ();
+		switchRenderer = GetComponent<UnipolarSwitchRenderer> ();
+	}
+
+	void OnMouseUp ()
+	{
+		switchRenderer.ChangeSwitch (UnipolarSwitchState.RELEASE);
+		squenceValidator.PushButton (gameObject);
+	}
+
+	void OnMouseDown() {
+		switchRenderer.ChangeSwitch (UnipolarSwitchState.PUSHED);
+	}
+
+
+}
